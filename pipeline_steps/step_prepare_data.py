65ca306f4a1f28a1832bd56793a14d61f8f86db2
Irvin Pérez <hotei.mat@gmail.com>
2024-10-12 19:09:26 -0300

    Applying same function to both dataframes.

--- pipeline_steps/step_prepare_data.py
+++ pipeline_steps/step_prepare_data.py
@@ -1,8 +1,17 @@
 import pandas as pd
 
 
-path_tabular_train: str = '/kaggle/input/child-mind-institute-problematic-internet-use/train.csv'
+def export_train_test_data(path: str, is_train: bool):
+    data: pd.DataFrame = pd.read_csv(path)
+    if is_train:
+        data.sii.fillna(0, inplace=True)
+    data.drop(columns=['id'], inplace=True)
+    partition_name = 'train' if is_train else 'test'
+    data.to_csv(f'output/{partition_name}_processed.csv', index=False)
 
-data_train: pd.DataFrame = pd.read_csv(path_tabular_train)
-data_train.sii.fillna(0, inplace=True)
-data_train.to_csv('train_processed.csv', index=False)
+# path_tabular_train: str = '/kaggle/input/child-mind-institute-problematic-internet-use/train.csv'
+path_tabular_train: str = 'data/child-mind-institute-problematic-internet-use/train.csv'
+path_tabular_test: str = 'data/child-mind-institute-problematic-internet-use/test.csv'
+
+for path_partition in zip([path_tabular_train, path_tabular_test], [True, False]):
+    export_train_test_data(*path_partition)
