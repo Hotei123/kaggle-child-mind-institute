b0cc93da64c2520db46bd2601107919faf01c22c
Irvin Pérez <hotei.mat@gmail.com>
2024-10-12 19:10:07 -0300

    Created section main.

--- pipeline_steps/step_prepare_data.py
+++ pipeline_steps/step_prepare_data.py
@@ -9,9 +9,12 @@ def export_train_test_data(path: str, is_train: bool):
     partition_name = 'train' if is_train else 'test'
     data.to_csv(f'output/{partition_name}_processed.csv', index=False)
 
-# path_tabular_train: str = '/kaggle/input/child-mind-institute-problematic-internet-use/train.csv'
-path_tabular_train: str = 'data/child-mind-institute-problematic-internet-use/train.csv'
-path_tabular_test: str = 'data/child-mind-institute-problematic-internet-use/test.csv'
 
-for path_partition in zip([path_tabular_train, path_tabular_test], [True, False]):
-    export_train_test_data(*path_partition)
+if __name__ == '__main__':
+
+    # path_tabular_train: str = '/kaggle/input/child-mind-institute-problematic-internet-use/train.csv'
+    path_tabular_train: str = 'data/child-mind-institute-problematic-internet-use/train.csv'
+    path_tabular_test: str = 'data/child-mind-institute-problematic-internet-use/test.csv'
+
+    for path_partition in zip([path_tabular_train, path_tabular_test], [True, False]):
+        export_train_test_data(*path_partition)
