098b92ea906b1732783abf25c17fa46b09809dd9
Irvin Pérez <hotei.mat@gmail.com>
2024-10-22 06:51:12 -0300

    Using time series 'describe'.

--- pipeline_steps/step_prepare_data.py
+++ pipeline_steps/step_prepare_data.py
@@ -1,7 +1,9 @@
 import os
 from typing import Any, Dict, List, Union
+import numpy as np
 import pandas as pd
 import pathlib
+from tqdm import tqdm
 
 
 class StepPrepareData:
@@ -17,8 +19,6 @@ class StepPrepareData:
     def get_partition_prepared(self, path: str, is_train: bool, 
                                use_target_nan: bool) -> pd.DataFrame:
         data: pd.DataFrame = pd.read_csv(path)
-        # if is_train and use_target_nan:
-        #     data[self.var_target].fillna(0, inplace=True)
         ids: List[str] = data['id'].tolist()
         data.drop(columns=['id'], inplace=True)
         data_dummy: pd.DataFrame = pd.get_dummies(data[self.vars_cat])
@@ -27,13 +27,20 @@ class StepPrepareData:
         if self.vars_cat_dummy is None:
             self.vars_cat_dummy = data_dummy.columns.tolist()
 
-        # path_series = pathlib.Path(path).parent
-        # if is_train:
-        #     path_series = path_series.joinpath('series_train.parquet')
-        # else:
-        #     path_series = path_series.joinpath('series_test.parquet')
-        # for id_child in ids:
-        #     path_child = path_series.joinpath(f'id={id_child}')
+        data_series = np.zeros((data.shape[0], 96))
+        path_series = pathlib.Path(path).parent
+        if is_train:
+            path_series = path_series.joinpath('series_train.parquet')
+        else:
+            path_series = path_series.joinpath('series_test.parquet')
+        for row_count, id_row in tqdm(enumerate(ids), total=len(ids)):
+            path_child = path_series.joinpath(f'id={id_row}/part-0.parquet')
+            if path_child.exists():
+                data_series_row = pd.read_parquet(path_child)
+                data_series_row = data_series_row.describe()
+                data_series_row.drop(columns='step', inplace=True)
+                data_series_row = data_series_row.values.reshape((1, -1))
+                data_series[row_count, :] = data_series_row
 
         return data
 
@@ -49,5 +56,7 @@ if __name__ == '__main__':
     # TODO: check if all variables, numerical or categorical, are included.
     # TODO: Fill Nans in training sii with predictions of algorithm trained in available sii.
     # TODO: Use time series data (start with .describe())
+    # TODO: Check that there is not a series for each id.
+    # TODO: Check number of parquets per id.
     step_prepare_data = StepPrepareData()
     step_prepare_data.export_partitions()
