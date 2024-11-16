5639163248dbfa3508fba96c0600e0f01df35099
Irvin Pérez <hotei.mat@gmail.com>
2024-11-16 11:29:39 -0300

    Using functions in DL training.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import yaml
 from pipeline_deep.data_preparation.tfrecord_manager_child_mind import TFRecordManagerChildMind
+from pipeline_deep.stage_train import train
 
 
 if __name__ == '__main__':
 
-    pass
+    train()
 
 # TODO: Use the full time series with tensorflow data.Dataset.from_generator
 # TODO: Normalize the time series, possibly eliminating columns
